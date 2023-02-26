8d341a130a3f2a550231bee57ceed74b91a43f76
Yuozas <53146589+Yuozas@users.noreply.github.com>
2023-02-26 22:42:09 +0200

    General backpack scriptable object logic.  Mapped scriptable backpack.

---
+++ Yume/Assets/_Main/Scripts/Inventory/Scriptable_InventoryVisualData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Inventory/Visuals", order = 1)]
+public class Scriptable_InventoryVisualData : ScriptableObject
+{
+    [field: SerializeField] public Sprite Sprite { get; set; }
+}
