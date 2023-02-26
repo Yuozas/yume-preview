3db9d21bdbbb03e672485c7dd13871828f72c30c
Mantas <Ma.Vaiciulis@gmail.com>
2023-02-26 16:53:27 +0200

    Percentage.cs spacing and change variable to var.

--- Yume/Assets/_Main/Scripts/Percentage.cs
+++ Yume/Assets/_Main/Scripts/Percentage.cs
@@ -4,37 +4,37 @@ using System;
 
 public class Percentage
 {
-	public event Action<float> OnUpdated;
+    public event Action<float> OnUpdated;
 
-	MonoBehaviour _behaviour;
-	IEnumerator _ienumerator;
-	public Percentage(MonoBehaviour behaviour) => _behaviour = behaviour;
+    MonoBehaviour _behaviour;
+    IEnumerator _ienumerator;
+    public Percentage(MonoBehaviour behaviour) => _behaviour = behaviour;
 
-	void Stop()
-	{
-		if (_ienumerator == null) return;
+    void Stop()
+    {
+        if (_ienumerator == null) return;
 
-		_behaviour.StopCoroutine(_ienumerator);
-		_ienumerator = null;
-	}
+        _behaviour.StopCoroutine(_ienumerator);
+        _ienumerator = null;
+    }
 
-	public void Play(float duration, Action onCompleted = null)
-	{
-		Stop();
+    public void Play(float duration, Action onCompleted = null)
+    {
+        Stop();
 
-		_ienumerator = Co_Play(duration, onCompleted);
-		_behaviour.StartCoroutine(_ienumerator);
-	}
-	IEnumerator Co_Play(float duration, Action onCompleted = null)
-	{
-		float percentage = 0f;
-		while (percentage < 1f)
-		{
-			percentage += Time.deltaTime * 1f / duration;
-			OnUpdated?.Invoke(percentage);
-			yield return null;
-		}
-
-		onCompleted?.Invoke();
+        _ienumerator = Co_Play(duration, onCompleted);
+        _behaviour.StartCoroutine(_ienumerator);
+    }
+    IEnumerator Co_Play(float duration, Action onCompleted = null)
+    {
+        var percentage = 0f;
+        while (percentage < 1f)
+        {
+            percentage += Time.deltaTime * 1f / duration;
+            OnUpdated?.Invoke(percentage);
+            yield return null;
+        }
+        
+        onCompleted?.Invoke();
 	}
 }
