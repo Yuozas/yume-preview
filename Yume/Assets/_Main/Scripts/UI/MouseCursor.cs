4ed7c3ba01bbd5cb510a8d85938b2056137a6253
Yuozas <53146589+Yuozas@users.noreply.github.com>
2023-02-28 21:53:42 +0200

    Defocus game cursor unhidden fix.

--- Yume/Assets/_Main/Scripts/UI/MouseCursor.cs
+++ Yume/Assets/_Main/Scripts/UI/MouseCursor.cs
@@ -5,18 +5,28 @@ public class MouseCursor : MonoBehaviour
     [SerializeField] Sprite _defaultCursor;
     SpriteRenderer _spriteRenderer;
 
+    void OnDisable() => Cursor.visible = true;
+
     void OnEnable()
     {
-        Cursor.visible = false;
+        HideCursor();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         SetDefaultCursor();
     }
 
-    void OnDisable() => Cursor.visible = true;
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            HideCursor();
+    }
+
     void Update()
     {
         var mousePositionByCamera = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePositionByCamera.x, mousePositionByCamera.y, transform.position.z);
     }
+
     public void SetDefaultCursor() => _spriteRenderer.sprite = _defaultCursor;
+
+    void HideCursor() => Cursor.visible = false;
 }
