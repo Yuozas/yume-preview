8741e496b328bad030cc6b76900844f017a8e216
Mantas <Ma.Vaiciulis@gmail.com>
2023-03-06 20:45:08 +0200

    Adjusted.
    Added #if UNITY_EDITOR for BoxCollider2D reference because it's only used in the OnDrawGizmos method. If the collider is not referenced, the inspector shouldn't constantly throw errors, especially when gizmos are optional. It won't try to transition to another scene and throw an error if Scriptable_TransitionDestination is not referenced. Using TryGetComponent instead of GetComponent, because bool comparison is cheaper than a null comparison.

--- Yume/Assets/_Main/Scripts/Transitions/TransitionTrigger.cs
+++ Yume/Assets/_Main/Scripts/Transitions/TransitionTrigger.cs
@@ -4,18 +4,25 @@ public class TransitionTrigger : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] Scriptable_TransitionDestination _destination;
+
+#if UNITY_EDITOR
     [SerializeField] BoxCollider2D _collider;
+#endif
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        var transitionable = collision.GetComponent<Transitionable>();
-        if (transitionable == null) return;
+        if (_destination == null) return;
+
+        var detected = collision.TryGetComponent<Transitionable>(out var transitionable);
+        if (!detected) return;
 
         Transitioner.Instance.Transition(_destination);
     }
 
     void OnDrawGizmos()
     {
+        if (_collider == null) return;
+
         var colliderPosition = transform.position + (Vector3)_collider.offset;
 
         Gizmos.color = Color.red;
