bf77efdf5290962b4ac37f0e251ff458bac96845
Mantas <ma.vaiciulis@gmail.com>
2023-05-09 17:25:06 +0300

    Adjusted according to comments.

--- Yume/Assets/_Main/Scripts/Transitions/TransitionTrigger.cs
+++ Yume/Assets/_Main/Scripts/Transitions/TransitionTrigger.cs
@@ -3,25 +3,28 @@ using UnityEngine;
 public class TransitionTrigger : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] Scriptable_TransitionDestination _destination;
+    [SerializeField] private Scriptable_TransitionDestination _destination;
 
 #if UNITY_EDITOR
-    [SerializeField] BoxCollider2D _collider;
+    [SerializeField] private BoxCollider2D _collider;
 #endif
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (_destination == null) return;
+        if (_destination is null)
+            return;
 
         var detected = collision.TryGetComponent<Transitionable>(out var transitionable);
-        if (!detected) return;
+        if (!detected)
+            return;
 
         Transitioner.Instance.Transition(_destination);
     }
 
-    void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
-        if (_collider == null) return;
+        if (_collider is null)
+            return;
 
         var colliderPosition = transform.position + (Vector3)_collider.offset;
 
