ba16eadae2b95aed636b475b7fc5de7fe67b0e7e
Yuozas <53146589+Yuozas@users.noreply.github.com>
2023-05-19 00:32:42 +0300

    Added SwiftLocator and initialization class.

---
+++ Yume/Assets/_Main/Scripts/Initialization/Initialization.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using SwiftLocator.Services.ServiceLocatorServices;
+using SwiftLocator.Services.ScopedServices;
+
+public static class Initialization
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    public static void Initialize()
+    {
+        RegisterSingletonServices();
+        RegisterTransientServices();
+        ServiceLocator.Build();
+    }
+
+    private static void RegisterSingletonServices()
+    {
+        // Todo register scoped services.
+        //ServiceLocator.SingletonRegistrator;
+    }
+
+    private static void RegisterTransientServices()
+    {
+        // Todo register transient services.
+        //ServiceLocator.TransientRegistrator;
+    }
+}
