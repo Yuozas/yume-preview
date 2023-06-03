fbaab44d005b96403b015ff3eb46294c70e504ac
Mantas <ma.vaiciulis@gmail.com>
2023-06-03 12:47:01 +0300

    Testing script removed.

--- Yume/Assets/_Main/Scripts/Nodes/UnityNode.cs
+++ Yume/Assets/_Main/Scripts/Nodes/UnityNode.cs
@@ -1,18 +1,28 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
+#endif
 
 public class UnityNode
 {
     public readonly INode Node;
+
+#if UNITY_EDITOR
     public Vector3 Position;
+#endif
 
     public UnityNode(INode node, Vector3 position)
     {
         Node = node;
+
+#if UNITY_EDITOR
         SetPosition(position);
+#endif
     }
 
+#if UNITY_EDITOR
     public void SetPosition(Vector3 position)
     {
         Position = position;
     }
+#endif
 }
