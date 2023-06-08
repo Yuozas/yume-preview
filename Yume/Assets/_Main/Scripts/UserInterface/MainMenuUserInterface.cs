3b0ff2bbee6b045a249354efa57f21b9998e43ec
Yuozas <53146589+Yuozas@users.noreply.github.com>
2023-06-09 02:01:36 +0300

    Basic main menu ui prefab, scene.

--- Yume/Assets/_Main/Scripts/UserInterface/MainMenuUserInterface.cs
+++ Yume/Assets/_Main/Scripts/UserInterface/MainMenuUserInterface.cs
@@ -4,17 +4,76 @@ using UnityEngine.UIElements;
 
 public class MainMenuUserInterface : MonoBehaviour
 {
+    private ISaveManager _saveManager;
+    private VisualElement _buttonWrapper;
 
     void Awake()
     {
-        var uiDocument = GetComponent<UIDocument>();
-        var root = uiDocument.rootVisualElement;
-        var saveManager = ServiceLocator.SingletonProvider.Get<ISaveManager>();
-        if (!saveManager.AnySaveExists())
+        _saveManager = ServiceLocator.SingletonProvider.Get<ISaveManager>();
+
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var body = root.Q<VisualElement>("Body");
+        _buttonWrapper = body.Q<VisualElement>("MenuButtonsWrapper");
+
+        SetupContinueButton();
+        SetupNewGameButton();
+        SetupSettingsButton();
+        SetupQuitGameButton();
+    }
+
+    private void SetupNewGameButton()
+    {
+
+        var newGameButton = _buttonWrapper.Q<Button>("NewGameButton");
+        newGameButton.clicked += TriggerNewGame;
+    }
+
+    private void TriggerNewGame()
+    {
+        //var saveManager = ServiceLocator.SingletonProvider.Get<ISaveManager>();
+        //saveManager.DeleteAllSaves();
+        //SceneLoader.Load(SceneLoader.SceneName.Game);
+    }
+
+    private void SetupContinueButton()
+    {
+        var continueButton = _buttonWrapper.Q<Button>("ContinueButton");
+        
+        if (!_saveManager.AnySaveExists())
         {
-            var continueButton = root.Q<Button>("ContinueButton");
             continueButton.SetEnabled(false);
-            continueButton.AddToClassList("dim-button");
+            continueButton.AddToClassList("button-hidden");
         }
+        else
+        {
+            continueButton.clicked += TriggerContinue;
+        }
+    }
+
+    private void TriggerContinue()
+    {
+        //await SceneManager.LoadSceneAsync();
+    }
+
+    private void SetupSettingsButton()
+    {
+        var settingsButton = _buttonWrapper.Q<Button>("SettingsButton");
+        settingsButton.clicked += TriggerSettings;
+    }
+
+    private void TriggerSettings()
+    {
+        //SceneLoader.Load(SceneLoader.SceneName.Options);
+    }
+
+    private void SetupQuitGameButton()
+    {
+        var quitButton = _buttonWrapper.Q<Button>("QuitGameButton");
+        quitButton.clicked += TriggerQuitGame;
+    }
+
+    private void TriggerQuitGame()
+    {
+        Application.Quit();
     }
 }
