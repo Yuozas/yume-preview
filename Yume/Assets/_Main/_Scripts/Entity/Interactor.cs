aea0c4e8ba368f18d3bf9d1a86570120139c3656
Mantas <ma.vaiciulis@gmail.com>
2023-06-11 13:51:10 +0300

    Offset y position.

--- Yume/Assets/_Main/_Scripts/Entity/Interactor.cs
+++ Yume/Assets/_Main/_Scripts/Entity/Interactor.cs
@@ -3,10 +3,11 @@
 public class Interactor
 {
     public const float DEFAULT_INTERACTION_DISTANCE = 1f;
+    private const float VERTICAL_OFFSET = 0.05f;
 
     public bool TryInteract(Vector2 position, Vector2 direction, float distance)
     {
-        var hit = Physics2D.Raycast(position, direction, distance);
+        var hit = Physics2D.Raycast(position + (Vector2.up * VERTICAL_OFFSET), direction, distance);
 
         if (hit.collider is null)
             return false;
