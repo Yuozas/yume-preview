bbf7a9598e480ed3d811ae193ea529ccf326ef01
Mantas <ma.vaiciulis@gmail.com>
2023-06-13 21:14:15 +0300

    Rename.

--- Yume/Assets/_Main/_Scripts/Entity/MultipleInteractor.cs
+++ Yume/Assets/_Main/_Scripts/Entity/MultipleInteractor.cs
@@ -6,7 +6,7 @@ public class MultipleInteractor : IInteractor
     public bool TryInteract(Vector2 position, Vector2 direction, float distance)
     {
         var hits = Physics2D.RaycastAll(position + (Vector2.up * IInteractor.VERTICAL_OFFSET), direction, distance);
-        var interactable = hits
+        var closestInteractable = hits
             .OrderBy(hit => hit.distance)
             .SelectFirstValid(hit =>
                 hit.collider.GetComponent<IInteractable>() is IInteractable interactable && interactable.CanInteract()
@@ -14,10 +14,10 @@ public class MultipleInteractor : IInteractor
                     : null
             );
 
-        if (interactable is null)
+        if (closestInteractable is null)
             return false;
 
-        interactable.Interact();
+        closestInteractable.Interact();
         return true;
     }
 }
