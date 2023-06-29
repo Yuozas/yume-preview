b613796632ce490cff0b3aa528ca71eccde2f3f4
Yuozas <53146589+Yuozas@users.noreply.github.com>
2023-06-30 00:37:14 +0300

    Create backpack simple visual elements through code. Backpack item description.

--- Yume/Assets/_Main/_Scripts/UserInterface/BackpackUserInterface.cs
+++ Yume/Assets/_Main/_Scripts/UserInterface/BackpackUserInterface.cs
@@ -1,17 +1,11 @@
 using SwiftLocator.Services.ServiceLocatorServices;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class BackpackUserInterface : MonoBehaviour
 {
-    [field: SerializeField] private VisualTreeAsset Slot { get; set; }
-    [field: SerializeField] private VisualTreeAsset SlotContainer { get; set; }
-    [field: SerializeField] private Sprite ScrollbarSprite { get; set; }
-    [field: SerializeField] private Sprite ScrollbarLowButton { get; set; }
-    [field: SerializeField] private Sprite ScrollbarHighButton { get; set; }
-    [field: SerializeField] private Sprite ScrollbarDragger { get; set; }
+    [field: SerializeField] Sprite SlotSprite { get; set; }
 
     private ScrollView _scrollView;
 
@@ -24,6 +18,7 @@ public class BackpackUserInterface : MonoBehaviour
         var backpack = body.Q<VisualElement>("Backpack");
         _scrollView = backpack.Q<ScrollView>("SlotScrollView");
 
+        SetupSlotContainer();
         SetupSlots();
         SetupScrollView();
     }
@@ -32,28 +27,18 @@ public class BackpackUserInterface : MonoBehaviour
     {
         _scrollView.contentContainer.Add(_slotContainer);
         _scrollView.horizontalScrollerVisibility = ScrollerVisibility.Hidden;
+    }
 
-        //_scrollView.verticalScroller.Q<VisualElement>("unity-dragger-border").style.display = DisplayStyle.None;
-        //_scrollView.verticalScroller.Q<VisualElement>("unity-tracker").style.display = DisplayStyle.None;
-
-        //var scrollViewSlider = _scrollView.verticalScroller.Q<VisualElement>("unity-slider");
-        //scrollViewSlider.style.backgroundImage = new StyleBackground(ScrollbarSprite);
-
-        //var scrollViewDragger = _scrollView.verticalScroller.Q<VisualElement>("unity-dragger");
-        //scrollViewDragger.style.backgroundImage = new StyleBackground(ScrollbarDragger);
-        //scrollViewDragger.style.height = 20;
-
-        //var scrollViewLowButton = _scrollView.verticalScroller.Q<VisualElement>("unity-low-button");
-        //scrollViewLowButton.style.backgroundImage = new StyleBackground(ScrollbarLowButton);
-
-        //var scrollViewHighButton = _scrollView.verticalScroller.Q<VisualElement>("unity-high-button");
-        //scrollViewHighButton.style.backgroundImage = new StyleBackground(ScrollbarHighButton);
+    private void SetupSlotContainer()
+    {
+        _slotContainer = new VisualElement
+        {
+            name = "SlotContainer"
+        };
     }
 
     private void SetupSlots()
     {
-        _slotContainer = SlotContainer.CloneTree().Q("SlotContainer");
-
         using var activeRealm = ServiceLocator.GetSingleton<IRealmActiveSaveHelper>().GetActiveSave();
         var savedSlots = activeRealm.Get<ActiveCharacer>().Character.CharacterHasStorages.First().Storage.StorageSlots;
 
@@ -64,7 +49,7 @@ public class BackpackUserInterface : MonoBehaviour
 
     private VisualElement SetupSlot(StorageSlot storageSlot)
     {
-        var slot = Slot.CloneTree().Q("Slot");
+        var slot = CreateSlotVisualElement();
         if (storageSlot.Item is null)
             return slot;
 
@@ -73,6 +58,23 @@ public class BackpackUserInterface : MonoBehaviour
 
         var slotItem = slot.Q<VisualElement>("SlotItem");
         slotItem.style.backgroundImage = new StyleBackground();
+
+        return slot;
+    }
+
+    private VisualElement CreateSlotVisualElement()
+    {
+        var slot = new VisualElement
+        {
+            name = "Slot"
+        };
+
+        slot.style.backgroundImage = new StyleBackground(SlotSprite);
+
+        slot.contentContainer.Add(new VisualElement()
+        {
+            name = "SlotItem"
+        });
 
         return slot;
     }
