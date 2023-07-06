a7d4c306521b5ba67a6c7a7c2f348d5928a6dc2b
Mantas <ma.vaiciulis@gmail.com>
2023-07-06 13:54:03 +0300

    Seller script.

---
+++ Yume/Assets/Seller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Seller : Entity, IInteractable
+{
+    [Header("References")]
+    [SerializeField] private InteractionScriptableObject _interaction;
+
+    public bool CanInteract()
+    {
+        return _interaction is not null;
+    }
+
+    public void Interact()
+    {
+        _interaction.Interact();
+    }
+}
